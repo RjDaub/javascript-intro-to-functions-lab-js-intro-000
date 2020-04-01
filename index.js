c94c54963b875853f233be3507510e2775d3763a
Robert Daub <rjd2066@gmail.com>
2020-04-01 16:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   else if (string.toUpperCase())
     return "YES INDEED!"
-  else ("I love you, Grandma.")
+  else (string === "I love you, Grandma.")
     return "I love you, too."
   console.log(string)
 
